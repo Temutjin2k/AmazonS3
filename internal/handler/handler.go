bbf285c6d8309ad0b01ed096b500893f334ad7ae
tkoszhan <tamutdzhin2006@mail.ru>
2024-10-07 17:07:00 +0500

    Added deletion

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,11 +3,15 @@ package handler
 import (
 	"encoding/csv"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
-	"triple-s/model"
+
+	"triple-s/internal/model"
 )
+
+var ErrInvalidPath = errors.New("error deleting bucket")
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -20,18 +24,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println(err)
 		}
 	case "DELETE":
-		if r.URL.Path == "" {
-			http.Error(w, "Error deleting bucket", http.StatusInternalServerError)
-		}
-		path := "./data" + r.URL.Path
-		fmt.Println("Removing directory", path)
 
-		err := os.RemoveAll(path)
+		err := deleteBucket(w, r.URL.Path)
 		if err != nil {
 			fmt.Println(err)
-			return
 		}
-		fmt.Fprintf(w, "Bucket deleted successfully: %v", path)
 	}
 }
 
@@ -41,11 +38,21 @@ func createBucket(w http.ResponseWriter, urlPath string) {
 	if err != nil {
 		if os.IsExist(err) {
 			http.Error(w, "Bucket already exists", http.StatusConflict) // 409 Conflict
+			return
 		} else {
 			http.Error(w, fmt.Sprintf("Error creating Bucket: %v", err), http.StatusInternalServerError) // 500 Internal Server Error
+			return
 		}
 	} else {
-		fmt.Fprintf(w, "Bucket created successfully: %v", bucketName)
+		fmt.Fprint(w, "Bucket created successfully")
+	}
+
+	// Creating objects.csv for storing metadata
+	fmt.Println(bucketName)
+	err = os.WriteFile(bucketName+"/objects.csv", []byte("ObjectKey,Size,ContentType,LastModified"), 0o755)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error creating metadata: %v", err), http.StatusInternalServerError)
+		fmt.Println(err)
 	}
 }
 
@@ -84,5 +91,37 @@ func listOfBuckets(w http.ResponseWriter) error {
 	if err := xml.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, "Failed to encode XML response", http.StatusInternalServerError)
 	}
+	return nil
+}
+
+func deleteBucket(w http.ResponseWriter, urlPath string) error {
+	if urlPath == "/" {
+		http.Error(w, "Error deleting bucket", http.StatusBadRequest)
+		return ErrInvalidPath
+	}
+	path := "./data" + urlPath
+	fmt.Println("Removing directory", path)
+	// Check if path exists
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		http.Error(w, "Path does not exist", http.StatusNotFound)
+		return err
+	} else if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Error checking path", http.StatusInternalServerError)
+		return err
+	}
+
+	if !info.IsDir() {
+		http.Error(w, "Path is not a directory", http.StatusBadRequest)
+		return err
+	}
+
+	err = os.RemoveAll(path)
+	if err != nil {
+		http.Error(w, "error", http.StatusInternalServerError)
+		return err
+	}
+	fmt.Fprintf(w, "Bucket deleted successfully: %v", path)
 	return nil
 }
