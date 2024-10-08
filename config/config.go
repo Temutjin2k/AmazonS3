7f20e3a37e3fd6af276b8494679b08abaebc3d3c
tkoszhan <tamutdzhin2006@mail.ru>
2024-10-08 19:17:40 +0500

    Added errors and Handler to check what operation is gonna to use

--- config/config.go
+++ config/config.go
@@ -1,7 +1,17 @@
 package config
 
+import "errors"
+
 var (
 	Dir                  string // Storing Directory flag
 	BucketMetadataFields []byte = []byte("Name,CreationDate,LastModifiedTime,Status\n")
 	ObjectMetadataFields []byte = []byte("ObjectKey,Size,ContentType,LastModified\n")
+
+	HandlerBucketList string = "BucketListHandler"
+	HandlerBucket     string = "BucketHandler"
+	HandlerObject     string = "ObjectHandler"
+
+	ErrBucketExists       = errors.New("bucket already exists")
+	ErrBucketDoesNotExist = errors.New("bucket does not exists")
+	ErrInvalidPath        = errors.New("error deleting bucket")
 )
